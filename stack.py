22c74a2c3ad9b1150724a41c50930ebb50895427
Leo Pucong Han <ph2369@columbia.edu>
2014-11-23 12:50:05 -0800

    Added test cases for stack

--- stack.py
+++ stack.py
@@ -5,7 +5,7 @@ class Stack:
 	def push(self, value):
 		self.stack.append(value)
 		self.count += 1
-	def pop(self, value):
+	def pop(self):
 		if self.count == 0 or len(self.stack) == 0:
 			return None
 		else:
@@ -14,4 +14,23 @@ class Stack:
 	def size(self):
 		return self.count
 	def isEmpty(self):
-		return self.count == 0 		
+		return self.count == 0
+
+#Test cases
+stack = Stack()
+stack.push(2)
+stack.push(4)
+stack.push(6)
+stack.push(8)
+stack.push(10)
+stack.push(12)
+stack.push(14)
+print stack.pop(), 14
+print stack.size(), 6
+print stack.pop(), 12
+print stack.pop(), 10
+print stack.pop(), 8
+print stack.pop(), 6
+print stack.pop(), 4
+print stack.pop(), 2
+print stack.isEmpty(), True
