a3ae13a3aeb505e3f25458d1255055d5fd498a63
Pucong Han <puconghan@Pucongs-MacBook-Pro.local>
2014-11-23 12:35:20 -0800

    Added testing cases and improve merge sort and quick sort

--- merge_sort.py
+++ merge_sort.py
@@ -1,12 +1,12 @@
 def merge_sort(lst):
-	if lst < 2:
+    if len(lst) < 2:
 		return lst
-	else:
-		pivot = len(lst) // 2
-		return merge(merge_sort(lst[:pivot]), merge_sort(lst[pivot:]))
+    else:
+        pivot = len(lst) // 2
+        return merge(merge_sort(lst[:pivot]), merge_sort(lst[pivot:]))
 
 def merge(lst1, lst2):
-	count1, count = 0, 0
+	count1, count2 = 0, 0
 	temp = []
 	while count1 < len(lst1) and count2 < len(lst2):
 		if lst1[count1] < lst2[count2]:
@@ -22,3 +22,6 @@ def merge(lst1, lst2):
 		temp.append(lst2[count2])
 		count2 += 1
 	return temp
+
+#Test case
+print merge_sort([2,6,7,3,9,1,5]);
--- quick_sort.py
+++ quick_sort.py
@@ -13,9 +13,32 @@ def quick_sort(lst):
 				left.append(item)
 			elif item > first:
 				right.append(item)
-		return quick_sort(left) + first + quick_sort(right)
+		return quick_sort(left) + [first] + quick_sort(right)
+
+#Test case
+print quick_sort([2,6,7,3,9,1,5])
 
 #pivot is the element in the middle
+def quick_sort(lst):
+    if len(lst) == 0:
+        return []
+    elif len(lst) == 1:
+        return lst
+    else:
+        pivot = len(lst) // 2
+        left, right = [], []
+        element = lst.pop(pivot)
+        for item in lst:
+            if item < element:
+                left.append(item)
+            else:
+                right.append(item)
+        return quick_sort(left) + [element] + quick_sort(right)
+
+#Test case
+print quick_sort([2,6,7,3,9,1,5])
+
+#pivot is the element in the middle. Pointers moved from both ends [Improved version]
 def quick_sort(lst, l, r):
 	if len(lst) > 2 and r > l:
 		left, right = l, r
@@ -36,3 +59,7 @@ def quick_sort(lst, l, r):
 		if right > l:
 			quick_sort(lst, l, right)
 
+#Test case
+lst = [2,6,7,3,9,1,5];
+quick_sort(lst, 0, len(lst)-1)
+print lst
