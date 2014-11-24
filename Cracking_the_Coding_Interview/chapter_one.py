dbeef8c9720f9598dd2b86ecd809b0a28dc8d4c4
Leo Pucong Han <ph2369@columbia.edu>
2014-11-23 16:26:13 -0800

    Updated chapter_one solution and added test cases

--- Cracking_the_Coding_Interview/chapter_one.py
+++ Cracking_the_Coding_Interview/chapter_one.py
@@ -14,13 +14,21 @@ def has_all_unique_char(inputstring):
             return False
     return True
 
+#Test case
+print has_all_unique_char('abcdefg'), True
+print has_all_unique_char('abcdefgg'), False
+
 def has_all_unique_char(inputstring):
-    lenth = len(inputstring)
-    for i in xrange(lenth):
-        for j in xrange(i, lenth+1, 1):
+    length = len(inputstring)
+    for i in xrange(length):
+        for j in xrange(i+1, length, 1):
             if inputstring[i] == inputstring[j]:
                 return False
     return True
+
+#Test case
+print has_all_unique_char('abcdefg'), True
+print has_all_unique_char('abcdefgg'), False
 
 #O(n^2) Using a list data structure
 def has_all_unique_char(inputstring):
@@ -31,6 +39,10 @@ def has_all_unique_char(inputstring):
             return False
         list.append(item)
     return True
+
+#Test case
+print has_all_unique_char('abcdefg'), True
+print has_all_unique_char('abcdefgg'), False
 
 #O(n) Using a list data structure with assumption of ASCII input string
 def has_all_unique_char(inputstring):
@@ -45,19 +57,32 @@ def has_all_unique_char(inputstring):
                 charlist[ord(char)] = True
         return True
 
+#Test case
+print has_all_unique_char('abcdefg'), True
+print has_all_unique_char('abcdefgg'), False
+
 #O(n) Using a hash table (dictionary)
 def has_all_unique_char(inputstring):
     char_table = {}
-    for char in char_table:
+    for char in inputstring:
         if char in char_table:
             return False
         else:
             char_table[char] = True
     return True
 
+#Test case
+print has_all_unique_char('abcdefg'), True
+print has_all_unique_char('abcdefgg'), False
+
 def has_all_unique_char(inputstring):
     if len(inputstring) > 256: return False
     else: return len(set(inputstring)) == len(inputstring)
+
+#Test case
+print has_all_unique_char('abcdefg'), True
+print has_all_unique_char('abcdefgg'), False
+
 
 '''
 1.2 Implement a function that reverses a string
@@ -73,12 +98,18 @@ def reverse_string(inputstring):
         output += stack.pop(-1)
     return output
 
+#Test case
+print 'Reverse abc: ', reverse_string('abc')
+
 # O(n) Recursive solution
 def reverse_string(inputstring):
     if len(inputstring) == 0:
         return ''
     else:
         return inputstring[-1:] + reverse_string(inputstring[:-1])
+
+#Test case
+print 'Reverse abc: ', reverse_string('abc')
 
 '''
 1.3 Given two strings, write a method to decide if one is a permutation of the other
@@ -90,11 +121,16 @@ def check_permutation(str1, str2):
         return False
     else:
         for char in str1:
-            if str2.find(char) != -1:
+            if str2.find(char) == -1:
                 return False
             else:
-                str2.pop(-1)
+                import re
+                str2 = re.sub(char, '', str2)
         return True
+
+#Test case
+print check_permutation('abc', 'cba'), True
+print check_permutation('acd', 'abc'), False
 
 #Run time depend of sorting algorithm
 def check_permutation(str1, str2):
@@ -103,19 +139,27 @@ def check_permutation(str1, str2):
     else:
         return sorted(str1) == sorted(str2)
 
+#Test case
+print check_permutation('abc', 'cba'), True
+print check_permutation('acd', 'abc'), False
+
 #O(n) Use a hash table
 def check_permutation(str1, str2):
+    if len(str1) != len(str2):
+        return False
     dict1, dict2 = {}, {}
     for char in str1:
         dict1[char] = char
     for char in str2:
         dict2[char] = char
-    if len(str1) != len(str2):
-        return False
     for key in dict1:
         if not (key in dict2 and dict1[key] == dict2[key]):
             return False
     return True
+
+#Test case
+print check_permutation('abc', 'cba'), True
+print check_permutation('acd', 'abc'), False
 
 '''
 1.4 Write a method to replace all space in a string with '%20'
@@ -131,6 +175,9 @@ def replace_space(inputstring):
             result_list.append(char)
     return ''.join(result_list)
 
+#Test case
+print replace_space('abc def')
+
 #O(n) Using a queue
 def replace_space(inputstring):
     queue = []
@@ -145,6 +192,9 @@ def replace_space(inputstring):
     while len(queue) > 0:
         outputstring += queue.pop(0)
     return outputstring
+
+#Test case
+print replace_space('abc def')
 
 '''
 1.5 Implement a method to perform basic string compression using the counts of repeated characters
@@ -171,6 +221,31 @@ def simple_compress(inputstring):
     else:
         return outputstring
 
+#Test case
+print simple_compress('aabcccccaaa')
+
+def simple_compress(inputstring):
+    prev = ''
+    count = 0
+    newstring = ''
+    for i, char in enumerate(inputstring):
+        if prev == '' and count == 0:
+            prev = char
+            count = 0
+        if prev == char:
+            count += 1
+            if i == len(inputstring) - 1:
+                newstring = newstring + prev + str(count)
+        else:
+            newstring = newstring + prev + str(count)
+            prev = char
+            count = 1
+    return newstring
+
+#Test case
+print simple_compress('aabcccccaaa')
+
+
 '''
 1.6 Rotate a NxN matrix 90 degrees
     (assuming clockwise rotation)
@@ -190,12 +265,21 @@ class MatrixProcessor:
                 rowstring += (' '+str(cell)+' ')
             rowstring += ']\n'
         return rowstring
+    def printnewmatrix(self):
+        rowstring = ''
+        for row in self.newmatrix:
+            rowstring += '['
+            for cell in row:
+                rowstring += (' '+str(cell)+' ')
+            rowstring += ']\n'
+        return rowstring
     def rotate90degree(self):
         newmatrix = []
-        for i in xrange(self.c):
+        for c in xrange(self.c):
             tempcolumn = []
-            for j in xrange(self.r):
-                tempcolumn.append(self.matrix[j][i])
+            for r in xrange(self.r):
+                tempcolumn.append(self.matrix[r][c])
+            tempcolumn.reverse()
             newmatrix.append(tempcolumn)
         self.r = len(newmatrix)
         self.c = len(newmatrix[0])
@@ -211,18 +295,21 @@ class MatrixProcessor:
             for c in xrange(self.c):
                 self.matrix[index][c] = 0
         for index in zerocolumn:
-            for c in xrange(self.r):
-                self.matrix[c][index] = 0
+            for r in xrange(self.r):
+                self.matrix[r][index] = 0
+
+#Test cases
+matrix= MatrixProcessor([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]])
+print matrix
+matrix.rotate90degree()
+print matrix.printnewmatrix()
+zeromatrix= MatrixProcessor([[1,2,3,4],[5,6,7,8],[9,10,0,12],[13,14,15,16]])
+zeromatrix.zerorowcolumn()
+print zeromatrix
 
 '''
 1.8 Assume you have a method isSubstring which checks if one word is a substring of another. Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only one call to isSubstring (e.g.,"waterbottle" is a rotation of "erbottlewat").
 '''
-
-def check_rotation(s1, s2):
-    if len(s1) != len(s2):
-        return False
-    else:
-        return s1.find(s2) > -1
 
 def check_rotation(str1, str2):
     if len(str1) != len(str2):
@@ -243,3 +330,6 @@ def check_rotation(str1, str2):
             return True
         else:
             return False
+
+print check_rotation('waterbottle', 'erbottlewat'), True
+print check_rotation('waterbottle', 'erbottlewag'), False
